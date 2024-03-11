2fdcb200948346874fec2c0e423d97b9b7e8a933
Lucas Scharenbroich <lscharen@prowestgis.com>
2024-03-10 19:22:44 -0500

    Add missing file

---
+++ src/Externals.s
@@ -0,0 +1,8 @@
+SMBStart             EXT   ; Base address of the ROM.  Should be XX/8000 in the bank the ROM is loaded into
+NonMaskableInterrupt EXT   ; Called every VBL
+ExtIn                EXT
+ROMBase              EXT
+
+; Addresses in the blitter banks
+lite_base            EXT
+lite_base_2          EXT
