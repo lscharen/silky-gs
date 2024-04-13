5d31a15d9eea7042b540069b6ef4da0871231fee
Lucas Scharenbroich <lscharen@prowestgis.com>
2024-04-13 16:49:34 -0500

    Remove files before copying

--- build-image.bat
+++ build-image.bat
@@ -8,9 +8,9 @@ set CADIUS="%1"
 set IMAGE="%2"
 set FOLDER="%3"
 
-REM Cadius does not overwrite files, so clear the root folder first
-%CADIUS% DELETEFOLDER %IMAGE% %FOLDER%
-%CADIUS% CREATEFOLDER %IMAGE% %FOLDER%
+REM Cadius does not overwrite files, so clear the files first
+%CADIUS% DELETEFILE %IMAGE% %FOLDER%SuperMarioGS
+%CADIUS% DELETEFILE %IMAGE% %FOLDER%BalloonFgtGS
 
 REM Now copy files and folders as needed
 %CADIUS% ADDFILE %IMAGE% %FOLDER% .\src\games\smb\SuperMarioGS
